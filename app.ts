9aa7c75d610f883daf7b93093fb91bd3f74d1d4c
irfanghat <irfanghat@gmail.com>
2023-04-29 17:09:48 +0300

    progress bar with classes

--- app.ts
+++ app.ts
@@ -1,30 +1,50 @@
 // DEBUGGING
 const log = console.log;
 
-// SELECTORS
-const progressBarContainer = document.querySelector('.progress-bar-container') as HTMLElement;
 
-const date = new Date();
 
-let seconds = date.getSeconds();
+// const date = new Date();
+// GET SECONDS
+// let seconds = date.getSeconds();
 
-let count = 1;
-function displaySeconds() {
-    if (seconds == 60) {
-        count = 1;
-    } else {
-        count++;
-        log(count)
+// let count = 1;
+// FUNCTION TO DISPLAY SECONDS
+// function displaySeconds() {
+//     if (seconds == 60) {
+//         count = 1;
+//     } else {
+//         count++;
+//         log(count)
+//     }
+// }
+
+class Time {
+    static displaySeconds() {
+        // SELECTORS
+        const progressBarContainer = document.querySelector('.progress-bar-container') as HTMLElement;
+        const date = new Date();
+        // GET SECONDS
+        let seconds:number = date.getSeconds();
+
+        let count:number = 0;
+        if (count <= 0) {
+            count++;
+            log(count)
+        }   
+            
+
+            // PROGRES BAR CONTENT STRUCTURE
+    //     let progressBarContent = `
+    //     <label for="file">Task in progress:</label>
+    //     <progress id="progress-bar" value=${count} max="100"> 50% </progress>
+    // `;
+    // APPEND PROGRESS BAR CONTENT TO PROGRESS BAR CONTAINER
+    // progressBarContainer.innerHTML = progressBarContent;
+
     }
 }
 
 setInterval(() => {
-    displaySeconds();
+    Time.displaySeconds();
 }, 1000)
 
-let progressBarContent = `
-    <label for="file">Task in progress:</label>
-    <progress id="progress-bar" value=${count} max="100"> 50% </progress>
-`;
-
-progressBarContainer.innerHTML = progressBarContent;
--- build/app.js
+++ build/app.js
@@ -1,27 +1,40 @@
 "use strict";
 // DEBUGGING
 const log = console.log;
-// SELECTORS
-const progressBarContainer = document.querySelector('.progress-bar-container');
-const date = new Date();
-let seconds = date.getSeconds();
-let count = 1;
-function displaySeconds() {
-    if (seconds == 60) {
-        count = 1;
-    }
-    else {
-        count++;
-        log(count);
-        let progressBarContent = `
-    <label for="file">Task in progress:</label>
-    <progress id="progress-bar" value=${count} max="100"> 50% </progress>
-`;
-        progressBarContainer.innerHTML = progressBarContent;
+// const date = new Date();
+// GET SECONDS
+// let seconds = date.getSeconds();
+// let count = 1;
+// FUNCTION TO DISPLAY SECONDS
+// function displaySeconds() {
+//     if (seconds == 60) {
+//         count = 1;
+//     } else {
+//         count++;
+//         log(count)
+//     }
+// }
+class Time {
+    static displaySeconds() {
+        // SELECTORS
+        const progressBarContainer = document.querySelector('.progress-bar-container');
+        const date = new Date();
+        // GET SECONDS
+        let seconds = date.getSeconds();
+        let count = 0;
+        if (count <= 0) {
+            count++;
+            log(count);
+        }
+        // PROGRES BAR CONTENT STRUCTURE
+        //     let progressBarContent = `
+        //     <label for="file">Task in progress:</label>
+        //     <progress id="progress-bar" value=${count} max="100"> 50% </progress>
+        // `;
+        // APPEND PROGRESS BAR CONTENT TO PROGRESS BAR CONTAINER
+        // progressBarContainer.innerHTML = progressBarContent;
     }
 }
 setInterval(() => {
-    displaySeconds();
+    Time.displaySeconds();
 }, 1000);
-
-
